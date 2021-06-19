8b18cf0e19feca063f1bcdc481f5754f3c36044a
Tchaklaka <53322938+Tchaklaka@users.noreply.github.com>
2021-06-19 20:57:00 +0200

    encore des commentaires

--- components/api.py
+++ components/api.py
@@ -10,6 +10,7 @@ import pandas as pd
 def get_current_weather_results(weather_dict: dict):
     """Extraction des données currentes du dictionnaire des résultats météo global"""
 
+    # Construction et mise en forme du DataFrame
     current = pd.Series(weather_dict['current'])
     current['dt'] = dt.datetime.fromtimestamp(current['dt'])
     current['sunrise'] = dt.datetime.fromtimestamp(current['sunrise'])
@@ -23,6 +24,7 @@ def get_current_weather_results(weather_dict: dict):
 def get_hourly_weather_results(weather_dict: dict):
     """Extraction des données horaires prévisionnelles du dictionnaire des résultats météo global"""
 
+    # Construction et mise en forme du DataFrame
     hourly = pd.DataFrame(weather_dict['hourly'])
     hourly.loc[:,'weather_condition'] = hourly['weather'].map(lambda ls: ls[0]['main'])
     hourly.loc[:,'weather_icon'] = hourly['weather'].map(lambda ls: ls[0]['icon'])
@@ -34,8 +36,11 @@ def get_hourly_weather_results(weather_dict: dict):
 def get_weather_results(lat: float, lon: float, one_call_api_base_url: str, api_key: str):
     """Obtention des résultats météo currents et prévisionnels"""
     
+    # Appel API
     url = one_call_api_base_url.format(lat, lon, api_key)
     response = json.loads(requests.get(url).text)
+
+    # Lecture de la réponse, mise en forme des résultats en cas de succès
     if response.get('cod') and response.get('cod') == 429:
         raise Exception("Compte OpenWeather bloqué et pas de dictionnaire par défaut")
     else:
